5b05c8dcca6b14dc27a9e6c19f366519739bce14
Ierzi <ierziytb@gmail.com>
2024-01-24 17:11:10 +0100

    Reworked in-file documentation

--- __main__.py
+++ __main__.py
@@ -14,20 +14,28 @@ if not sys.version_info.major >= 3:
 # A chess engine I made for fun, trying to keep the code as simple and documented as possible.
 # This is the modern version of my very first chess engine, ButiEngine, which didn't work. :(
 # --------------------------------------------------------------------------------------
-# The creation started on January 22nd, 2024. There weren't much, just the main class (which was empty)
-# I really started creating it two days later, it came with the king square table and the main class
+# The creation started on January 23rd, 2024. There weren't much, just the main class (which was empty)
+# I really started creating it one day later, it came with the king square table and the main class.
+# Go see the commits history on the GitHub repository to know to full timeline.
 
 # The main class of the engine
 class ButiEngine:
     def __init__(self):
         """The modern version of my chess engine."""
         # Older private versions used a variable for the piece square tables
-        # Now, this is moved to the piecetables/ folder.
+        # Now, this is moved to the piecesquaretables/ folder.
 
-    def evaluate(self):
-        pass
+    def evaluate(self, board: list[list[str]]):
+        evaluation = 0
+        fen = self.board_to_fen(board)
 
     def best_move(self):
+        pass
+
+    @staticmethod
+    def board_to_fen(self, board: list[list[str]]) -> str:
+        # The board to fen code is "stolen" from the original ButiEngine, which I used in MixusEngine too.
+        # It works, and that's the essential.
         pass
 
 
